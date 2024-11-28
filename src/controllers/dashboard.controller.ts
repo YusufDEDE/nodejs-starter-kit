9258542788c4535f5543b881ae4d7d533bc96ac1
Yusuf Dede <yusufdede95@hotmail.com>
2024-11-28 07:01:12 +0300

    update your email

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from 'express';
+import {verifyAccessToken} from "../utils/jwt.utils";
 
 
 
 export const dashboard = async (req: Request, res: Response): Promise<void> => {
     try {
-        res.status(200).json({"message": "Welcome Dashboard! - I see your token!"});
+        const authHeader = req.headers.authorization;
+        // @ts-ignore
+        const token = authHeader.split(' ')[1];
+        const decode = verifyAccessToken(token);
+
+        // @ts-ignore
+        res.status(200).json({"message": "Welcome Dashboard! - I see your token!", "your_mail": decode.email});
     } catch (error) {
         console.error('Error fetching users:', error);
         res.status(500).json({ error: 'Internal server error' });
